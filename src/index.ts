9dc1797f68fbeae1a7fa0648f7acdf802f8b1659
Django Shelton <djangoshelton@gmail.com>
2021-03-14 16:15:53 +0000

    add graphql server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,37 @@
-const world = "world";
+import { loadSchemaSync } from "@graphql-tools/load";
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import { addResolversToSchema } from "@graphql-tools/schema";
+import { ApolloServer } from "apollo-server";
+import { Todo } from "./types/types";
 
-export function hello(word: string): void {
-  console.log(`Hello ${word}! `);
-}
+const todos: Todo[] = [
+  {
+    title: "Walk dog",
+    description: "Before 9am",
+  },
+  {
+    title: "Clean bathroom",
+    description: "",
+  },
+];
 
-hello(world);
+const schema = loadSchemaSync("**/*.graphql", {
+  loaders: [new GraphQLFileLoader()],
+});
+
+const resolvers = {
+  Query: {
+    todos: () => todos,
+  },
+};
+
+const schemaWithResolvers = addResolversToSchema({
+  schema,
+  resolvers,
+});
+
+const server = new ApolloServer({ schema: schemaWithResolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
